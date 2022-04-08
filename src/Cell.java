dcb21b54dc7a760e877a05c59797359ad04fa6e6
Hans Teh <n10530851@qut.edu.au>
2022-04-08 20:43:07 +1000

    Implemented Print, RemoveWallX, RemoveWallY, GetOpenNeighbours

--- src/Cell.java
+++ src/Cell.java
@@ -6,6 +6,22 @@ public class Cell {
     private int[] walls = {1, 1, 1, 1};  // North, East, South, West
 
     /**
+     * Getter for x.
+     * @return x.
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * Getter for y.
+     * @return y.
+     */
+    public int getY() {
+        return y;
+    }
+
+    /**
      * Calculates the distance between this and the target cell.
      * @param targetCell The target cell.
      * @return The distance between current cell and target cell in cells.
@@ -40,6 +56,10 @@ public class Cell {
         {
             neighbours.add(cells[y-1][x]);
         }
+        if (x < sizeX)
+        {
+            neighbours.add(cells[y][x+1]);
+        }
         if (y < sizeY)
         {
             neighbours.add(cells[y+1][x]);
@@ -48,13 +68,37 @@ public class Cell {
         {
             neighbours.add(cells[y][x-1]);
         }
-        if (x < sizeX)
+
+        return neighbours;
+    }
+
+    public ArrayList<Cell> GetOpenNeighbours(Maze maze)
+    {
+        ArrayList<Cell> neighbours = new ArrayList<>();
+        Cell[][] cells = maze.getCells();
+        int sizeX = maze.getSizeX();
+        int sizeY = maze.getSizeY();
+
+        if (y > 0 && walls[0] == 0)
+        {
+            neighbours.add(cells[y-1][x]);
+        }
+        if (x < sizeX && walls[1] == 0)
         {
             neighbours.add(cells[y][x+1]);
+        }
+        if (y < sizeY && walls[2] == 0)
+        {
+            neighbours.add(cells[y+1][x]);
+        }
+        if (x > 0 && walls[3] == 0)
+        {
+            neighbours.add(cells[y][x-1]);
         }
 
         return neighbours;
     }
+
 
     /**
      * Removes a wall on the x-axis.
@@ -62,7 +106,14 @@ public class Cell {
      */
     public void RemoveWallX(int wall)
     {
-
+        if (wall == 1)
+        {
+            walls[1] = 0;
+        }
+        else if (wall == -1)
+        {
+            walls[3] = 0;
+        }
     }
 
     /**
@@ -71,22 +122,20 @@ public class Cell {
      */
     public void RemoveWallY(int wall)
     {
-
+        if (wall == 1)
+        {
+            walls[2] = 0;
+        }
+        else if (wall == -1)
+        {
+            walls[0] = 0;
+        }
     }
 
-    /**
-     * Getter for x.
-     * @return x.
-     */
-    public int getX() {
-        return x;
-    }
-
-    /**
-     * Getter for y.
-     * @return y.
-     */
-    public int getY() {
-        return y;
+    public void Print()
+    {
+        System.out.format("1  %d  1\n", walls[0]);
+        System.out.format("%d     %d\n", walls[3], walls[1]);
+        System.out.format("1  %d  1\n", walls[2]);
     }
 }
