91e6df87c0becb562969ddec43cf3ad8ab246bb6
Hans Teh <n10530851@qut.edu.au>
2022-04-09 15:28:18 +1000

    Fixed CellNode compareTo, Implemented equals and toString

--- src/CellNode.java
+++ src/CellNode.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class CellNode implements Comparable<CellNode> {
     private Cell cell;
     private CellNode parent = null;
@@ -41,15 +43,30 @@ public class CellNode implements Comparable<CellNode> {
      * Compares the combined cost of this node and a target node.
      * @param targetNode The target node.
      * @return 0 if same, -1 if less than, 1 if more than.
+     * TODO Implementation is backwards, but for some reason orders correctly on Priority Queue
      */
     @Override
     public int compareTo(CellNode targetNode) {
         if (combinedCost == targetNode.combinedCost) {
             return 0;
         } else if (combinedCost < targetNode.combinedCost) {
-            return -1;
-        } else {
             return 1;
+        } else {
+            return -1;
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o.getClass() == this.getClass())
+        {
+            if (this.cell == ((CellNode) o).cell) return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return this.cell.toString();
     }
 }
