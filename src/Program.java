c3bc0cbb6c54ea9034eb81ccf85f50c3f09c3970
Hans Teh <n10530851@qut.edu.au>
2022-04-09 15:30:32 +1000

    Testing maze GenerateMaze and Solve

--- src/Program.java
+++ src/Program.java
@@ -1,6 +1,29 @@
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
 public class Program {
     public static void main(String[] args)
     {
+        System.out.println();
+
+        int sizeX = 10;
+        int sizeY = 5;
+        Maze testMaze = new Maze(sizeX, sizeY);
+        testMaze.Print();
+
+        System.out.println();
+
+        testMaze.GenerateMaze();
+        testMaze.Print();
+
+        System.out.println();
+
+        ArrayList<CellNode> solution = testMaze.Solve(0, 0, sizeX - 1, 0);
+        for (CellNode node:solution)
+        {
+            System.out.print(node.toString() + " ");
+        }
+        System.out.println();
 
     }
 }
