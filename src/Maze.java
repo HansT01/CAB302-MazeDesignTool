1023153c67b66b4dd3c9ba4822b4549c5268b0f8
Hans Teh <n10530851@qut.edu.au>
2022-04-22 01:17:38 +1000

    Moved Program main to Maze main method

--- src/Maze.java
+++ src/Maze.java
@@ -382,4 +382,41 @@ public class Maze {
         }
         return deadEndCount;
     }
+
+    public static void main(String[] args) {
+        long startTime;
+        long endTime;
+
+        startTime = System.nanoTime();
+        Maze testMaze = new Maze(20, 10);
+        endTime = System.nanoTime();
+        long constructTime = endTime - startTime;
+
+        MazeImage testImage = new MazeImage(0, 3, 3);
+        testMaze.PlaceImage(testImage, 2, 2);
+
+        startTime = System.nanoTime();
+        testMaze.GenerateMaze();
+        endTime = System.nanoTime();
+        long generateTime = endTime - startTime;
+
+        testMaze.Print();
+        System.out.println();
+
+        startTime = System.nanoTime();
+        CellNode[] solution = testMaze.Solve();
+        endTime = System.nanoTime();
+        long solveTime = endTime - startTime;
+
+        for (CellNode node:solution)
+        {
+            System.out.print(node.toString() + " ");
+        }
+        System.out.println();
+        System.out.println();
+
+        System.out.format("Construction time (ms): %f\n", constructTime/1000000.0);
+        System.out.format("Generation time (ms): %f\n", generateTime/1000000.0);
+        System.out.format("Solve time (ms): %f\n", solveTime/1000000.0);
+    }
 }
