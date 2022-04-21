e17297e07aed4d4991be0fcd7bc1196d99d81a1c
calebbarnard <103184005+calebbarnard@users.noreply.github.com>
2022-04-21 20:39:50 +1000

    Create LoginUI class

---
+++ src/LoginUI.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LoginUI extends JFrame implements Runnable{
+
+
+    @Override
+    public void run() { }
+}
