e84c7365d47e49bf23c204bdc03d5a769caa415c
AdamQUTDev <n10530631@qut.edu.au>
2022-06-05 19:08:49 +1000

    Create DatabaseConnection.java

---
+++ src/DatabaseConnection.java
@@ -0,0 +1,15 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class DatabaseConnection {
+    private Connection connection;
+
+    public DatabaseConnection() {
+        try {
+            String url = "url";
+            String username = "username";
+            String password = "password";
+    }
+}
